a5f9b4a96ddc394a86709cd8b1ec38b441be0da8
Jimmy <jimmy.ihalainen@gmail.com>
2022-11-10 09:44:40 +0200

    Add project directory to sys.path for auto-instrumentation

--- manage.py
+++ manage.py
@@ -14,6 +14,15 @@ if __name__ == "__main__":
         os.environ.setdefault("APLUS_LOCAL_SETTINGS", "aplus/local_settings.test.py")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aplus.settings")
 
+    # Add application directory to PYTHONPATH for auto-instrumentation
+    try:
+        app_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        user_paths = os.environ["PYTHONPATH"].split(os.pathsep)
+        if not app_path in user_paths:
+            os.environ["PYTHONPATH"] += ':' + app_path
+    except KeyError:
+        pass
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
